25f18cde9c5cdf427346b4b5a02d92aee27e1d85
kimbank <kimeunhang@inha.edu>
2025-07-03 19:29:08 +0900

    feat: MyCar 페이지 및 사용자 프로필 컴포넌트 추가

---
+++ frontend/src/app/(mycar)/mycar/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import AppBar from "@/components/app-bar";
+import BottomNavigation from "@/components/bottom-navigation";
+import useAuthStore from "@/store/auth";
+import { AuthAlertDialog } from "@/components/auth-alert-dialog";
+import UserProfile from "./user-profile";
+
+export default function MyCar() {
+  const { isAuthenticated } = useAuthStore();
+  if (!isAuthenticated) {
+    return <AuthAlertDialog />;
+  }
+
+  return (
+    <div className="min-h-screen">
+      <AppBar />
+      <main className="flex-col justify-center items-center pt-14 pb-20">
+        <UserProfile />
+      </main>
+      <BottomNavigation />
+    </div>
+  );
+}
